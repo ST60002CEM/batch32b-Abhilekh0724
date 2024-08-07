8aec1b0aacc0349121c15482b30e2b8117131f3e
Abhilekh0724 <nimwang0724@gmail.com>
2024-08-07 19:05:37 +0545

    updating

--- lib/app/constants/api_endpoint.dart
+++ lib/app/constants/api_endpoint.dart
@@ -3,18 +3,32 @@ class ApiEndpoints {
 
   static const Duration connectionTimeout = Duration(seconds: 1000);
   static const Duration receiveTimeout = Duration(seconds: 1000);
-  // static const String baseUrl = "http://192.168.1.68/api/";
-  static const String baseUrl = "http://10.0.2.2:5500/api/";
-  //static const String baseUrl = "http://192.168.4.4:3000/api/v1/";
+  static const String baseUrl = "http://192.168.1.70:5500/api/";
 
   // ====================== Auth Routes ======================
   static const String login = "user/login";
   static const String register = "user/create";
-  static const String homepage = "user/get";
   static const String imageUrl = "http://10.0.2.2:3000/uploads/";
-  static const String uploadImage = "auth/uploadImage";
   static const String currentUser = "auth/getMe";
-  static const String getAllUser = "auth/getAllUsers";
-  static const String updateUser = "auth/updateUser/";
-  static const String deleteUser = "auth/deleteUser/";
+
+  // ====================== Profile Routes ======================
+  static const String profile = "profile/";
+  static const String uploadProfilePic = "profile/uploadProfilePic";
+
+  // ====================== Admin Routes ======================
+  static const String createCategory = "admin/create";
+  static const String getAllCategories = "admin/get";
+  static const String updateCategory = "admin/update/";
+  static const String deleteCategory = "admin/delete/";
+  static const String searchCategory = "admin/search";
+  // static const String getCategoryById = "admin/get/";
+
+  // ====================== Review Routes ======================
+  static const String review = "review/reviews";
+  static const String getReviewsByCategory = "review/reviews/";
+
+  // ====================== Book Routes ======================
+  static const String book = "book/book";
+  static const String getBookingsByCategory = "book/category/";
+  static const String getBookingsByUser = "book/bookeduser";
 }
