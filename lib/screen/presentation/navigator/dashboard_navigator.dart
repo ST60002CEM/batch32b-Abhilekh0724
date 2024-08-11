bb776a2ab3fa814f578aab31b5270be66d5ff25f
Abhilekh0724 <nimwang0724@gmail.com>
2024-08-11 20:22:55 +0545

    update

---
+++ lib/screen/presentation/navigator/dashboard_navigator.dart
@@ -0,0 +1,20 @@
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import '../../../../../app/navigator/navigator.dart';
+// import '../../../features/home/presentation/presentation/view/search_view.dart';
+// import '../view/bottom_view/dashboard_view.dart';
+//
+// final dashboardViewNavigatorProvider = Provider<DashboardViewNavigator>((ref) {
+//   return DashboardViewNavigator();
+// });
+//
+// class DashboardViewNavigator with DashboardViewRoute {
+//   void openSearchPage() {
+//     NavigateRoute.pushRoute(const SearchPage());
+//   }
+// }
+//
+// mixin DashboardViewRoute {
+//   void openDashboardView() {
+//     NavigateRoute.popAndPushRoute(const DashboardView());
+//   }
+// }
