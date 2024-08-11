154eaa32da418a7bef8c3722c749ac9da7c94839
Abhilekh0724 <nimwang0724@gmail.com>
2024-08-11 20:23:58 +0545

    category detail

--- lib/features/home/presentation/data/data_source/local/category_local_data_source.dart
+++ lib/features/home/presentation/data/data_source/local/category_local_data_source.dart
@@ -4,4 +4,6 @@ import '../../../domain/entity/venue_card_entity.dart';
 abstract class CategoryLocalDataSource {
   Future<List<VenueCardEntity>> getCategories();
   Future<void> saveCategories(List<VenueCardEntity> categories);
+
+
 }
