fd5aec27ed34e620980e27b155d957ca3e02e535
Abhilekh0724 <nimwang0724@gmail.com>
2024-08-16 14:54:04 +0545

    for booking

---
+++ lib/features/book/data/dto/booking_dto.dart
@@ -0,0 +1,26 @@
+// import 'package:json_annotation/json_annotation.dart';
+//
+// part 'booking_dto.g.dart';
+//
+// @JsonSerializable()
+// class BookingDTO {
+//   final String id;
+//   final String categoryId;
+//   final String userId;
+//   final DateTime bookingDate;
+//   final String status;
+//   final DateTime createdAt;
+//
+//   BookingDTO({
+//     required this.id,
+//     required this.categoryId,
+//     required this.userId,
+//     required this.bookingDate,
+//     required this.status,
+//     required this.createdAt,
+//   });
+//
+//   factory BookingDTO.fromJson(Map<String, dynamic> json) => _$BookingDTOFromJson(json);
+//
+//   Map<String, dynamic> toJson() => _$BookingDTOToJson(this);
+// }
